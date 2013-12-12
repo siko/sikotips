6b8920c2e84c90b35432b70fd15f1c68e9c5f95d
Siko Chen <sikocb@gmail.com>
2013-12-12 16:16:58 +0800

    update ripple

--- ubuntu/boinc.sh
+++ ubuntu/boinc.sh
@@ -1,9 +1,5 @@
 cd /root/BOINC/
-
 ./run_client --daemon
-#./boinccmd --project_attach http://www.worldcommunitygrid.org/ d07b8dfa74a29f457429a7191219a931
-sleep 100
-./boinccmd --project http://www.worldcommunitygrid.org/ allowmorework
 sleep 100
 ./boinccmd --project http://www.worldcommunitygrid.org/ update
 
